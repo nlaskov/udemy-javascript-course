c50016be2c5466debf6d2a9e2b4cdfef78792d6b
Nikola <nikola.laskov@codexio.bg>
2023-11-30 10:15:57 +0200

    First lecture exercise

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,8 @@
+//LECTURE: Values and Variables 
+var country = "Bulgaria";
+var continent = "Europe";
+var population = 6;
+
+console.log(country);
+console.log(continent);
+console.log(population);
