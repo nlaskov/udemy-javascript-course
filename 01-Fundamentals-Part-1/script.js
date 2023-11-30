2013a75468659f8273c5845bf5eb43859c644d57
Nikola <nikola.laskov@codexio.bg>
2023-11-30 14:44:23 +0200

    Add several more lecture exercises and first challenge

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,8 +1,29 @@
 //LECTURE: Values and Variables 
-var country = "Bulgaria";
-var continent = "Europe";
-var population = 6;
+const country = "Bulgaria";
+const continent = "Europe";
+const population = 7;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+//LECTURE: Data Types
+const isIsland = false;
+var language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//LECTURE: let, const and var
+language = "bulgarian";
+
+//LECTURE: Basic Operators
+console.log(population / 2);
+console.log(population + 1);
+console.log(6 < population);
+console.log(33 > population);
+
+const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
+console.log(description);
