9621f3faae02db4ace58948d8a5eb0afcdbb24f2
Nikola <nikola.laskov@codexio.bg>
2023-12-01 10:39:59 +0200

    Final challenge and lecture for 1st section

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -59,3 +59,29 @@ if (language === "english" && population < 50 && !isIsland) {
 } else {
     console.log(`${country} does't meet your criteria`);
 }
+
+//LECTURE: The switch Statement
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log("MOST number of native speakers!");
+        break;
+    case 'spanish':
+        console.log("2nd place in number of native speakers");
+        break;
+    case 'english':
+        console.log("3rd place");
+        break;
+    case 'hindi':
+        console.log("Number 4");
+        break;
+    case 'arabic':
+        console.log("5th most spoken language");
+        break;
+    default:
+        console.log("Great language too :D");
+}
+
+//LECTURE: The Conditional (Ternary) Operator
+
+population > 33 ? console.log(`${country}'s population is above avarage`) : console.log(`${country}'s population is below avarage`)
