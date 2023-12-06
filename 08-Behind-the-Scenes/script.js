ba0368a91d392e507dc6284bafda231f0bc92c38
Nikola <nikola.laskov@codexio.bg>
2023-12-06 13:43:14 +0200

    Behind the scense lectures code

---
+++ 08-Behind-the-Scenes/script.js
@@ -0,0 +1,39 @@
+// 'use strict';
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
+//   console.log(firstName);
+
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var milleninal = true;
+//       const firstName = 'Ivan';
+//       const str = `Oh, and you are a milleninal, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+
+//       output = 'New Output!';
+//     }
+
+//     // console.log(milleninal);
+//     // console.log(add(1, 2));
+//     console.log(output);
+//   }
+//   printAge();
+//   return age;
+// }
+
+// const firstName = 'Nikola';
+// calcAge(2000);
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Nikola';
+let job = 'IT';
+const year = 2000;
