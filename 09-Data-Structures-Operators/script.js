8ae74dd31f8b1a271fb11b21fd0cca1accc06ad7
Nikola <nikola.laskov@codexio.bg>
2023-12-07 12:41:26 +0200

    First challenege of Section 9

---
+++ 09-Data-Structures-Operators/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+//Challenge 1: Football game
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players;
+
+const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const { team1, draw, team2 } = game.odds;
+
+function printGoals(...players) {
+  for (let i = 0; i < players.length; i++) {
+    console.log(players[i]);
+  }
+  console.log(`${players.length} goals were scored!`);
+}
+printGoals(...game.scored);
+
+team1 < team2 && console.log(game.team1);
+team2 < team1 && console.log(game.team2);
