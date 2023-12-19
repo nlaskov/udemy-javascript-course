042d994d0c22d7b4fa92f0991e796d2c5f33f535
Nikola <nikola.laskov@codexio.bg>
2023-12-19 10:51:15 +0200

    Add third callenge

--- 11-Arrays-Bankist/challenge.js
+++ 11-Arrays-Bankist/challenge.js
@@ -20,13 +20,12 @@ const checkDogs = function (dogsJulia, dogsKate) {
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
-//Challenge 2
-const calcAverageHumanAge = function (ages) {
-  return (humanAges = ages
+//Challenge 2 + 3
+const calcAverageHumanAge = ages =>
+  ages
     .map(age => (age <= 2 ? age * 2 : age * 4 + 16))
     .filter(age => age >= 18)
-    .reduce((acc, age) => acc + age, 0));
-};
+    .reduce((acc, age) => acc + age, 0);
 
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
